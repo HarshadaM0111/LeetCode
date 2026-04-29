321bd433eb6c4b19f21c1d12f47bc4dfc09de2c9
harshadaM0111 <harshadamane0111@gmail.com>
2026-04-29 10:25:54 +0530

    swapnumber file created

---
+++ SwapTwoNumber.java
@@ -0,0 +1,24 @@
+package Leetcode;
+
+public class SwapTwoNumber {
+
+	public static void main(String[] args) {
+		
+		int a= 10 , b= 20, c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("Swap a & b : a = "+a+ " b = "+b);
+
+	}
+
+}
+
+
+
+/*
+ OUTPUT
+ 
+ Swap a & b : a = 20 b = 10
+
+*/
